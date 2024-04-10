bb4fdbe05e2da7b3b2962819b6e96eaf4c325dae
William Davis <williamdavis.dev@gmail.com>
2024-04-10 15:48:55 -0400

    dynamic index creation, style changes, prompt template changes for attribution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import os
+import json
+import fitz
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,21 +11,85 @@ from canopy.tokenizer import Tokenizer
 from canopy.context_engine import ContextEngine
 from canopy.chat_engine import ChatEngine
 from canopy.models.data_models import UserMessage, SystemMessage
+from canopy.knowledge_base.record_encoder import OpenAIRecordEncoder
+from canopy.models.data_models import Document
+from pinecone import Pinecone, PodSpec
+from canopy.knowledge_base import list_canopy_indexes
 
 Tokenizer.initialize()
-INDEX_NAME = "wealth-outlook"
 
-kb = KnowledgeBase(index_name=INDEX_NAME)
-kb.connect()
+st.title("RAG Demo")
 
-context_engine = ContextEngine(kb)
-chat_engine = ChatEngine(context_engine,allow_model_params_override=True)
+uploaded_files = st.file_uploader(label="Upload your files", key='upload_file',accept_multiple_files=True)
+if 'processed_files' not in st.session_state:
+    processed_files = {}
+else:
+    processed_files = st.session_state['processed_files']
+if uploaded_files is not None:
+    if len(uploaded_files) != len(processed_files) or len(processed_files) == 0:
+        processed_pages = []
+        for uploaded_file in uploaded_files:
+            if uploaded_file.name not in processed_files.keys():
+                with st.spinner(f"Reading {uploaded_file.name}..."):
+                    pdf_file = None
+                    processed_pages = None
+                    formatted_json_fp = uploaded_file.name.replace('.pdf','.json')
+                    if not os.path.exists(formatted_json_fp):
+                        with open(uploaded_file.name,'wb') as f:
+                            f.write(uploaded_file.getvalue())
+                        doc = fitz.open(uploaded_file.name)
+                        extracted_text = [page.get_text() for page in doc]
+                        data = []
+                        for i,page in enumerate(extracted_text):
+                            data.append({
+                                'id': str(i), 
+                                'text': page, 
+                                'source': f'{uploaded_file.name}: page {i+1}', 
+                                'metadata': {'title': uploaded_file.name,
+                                            'primary_category': 'Finance',
+                                            'published': 2024
+                                            },
+                            })
+                        with open(formatted_json_fp, 'w') as out_fp:
+                            json.dump(data,out_fp,indent=4)
+                    else:
+                        with open(formatted_json_fp, 'r') as json_file:
+                            data = json.load(json_file)
 
-st.title("Canopy RAG for 2024 Wealth Outlook")
+                processed_files[uploaded_file.name] = data
+                st.session_state['processed_files'] = processed_files
+            print("Reading Complete")
 
-st.file_uploader(label="Upload your files")
+if 'processed_files' in st.session_state.keys() and len(st.session_state['processed_files'].keys()) > 0:
+    len_files = len(st.session_state['processed_files'].keys())
+    selected_file = st.radio("Document for RAG", st.session_state['processed_files'].keys(), index=0,
+                             key="file_lookup_key",help="Document to use for RAG",horizontal=False)
+    if 'selected_file' not in st.session_state.keys():
+        st.session_state['selected_file'] = selected_file
+
+    fp = selected_file.replace('.pdf','')
+    idx_name = fp.replace(' ','-').lower()
+    if f'canopy--{idx_name}' in list_canopy_indexes():
+        kb = KnowledgeBase(index_name=idx_name)
+        kb.connect()
+        print(f'connected to {idx_name}')
+    else:
+        encoder = OpenAIRecordEncoder(model_name="text-embedding-3-small")
+        kb = KnowledgeBase(index_name=idx_name, record_encoder=encoder)
+        kb.create_canopy_index()
+        kb.connect()
+        print(f'connecting new index for {idx_name}...')
+        documents = [Document(id=line['id'],
+                        text=line['text'],
+                        source=line['source'],
+                        metadata=line['metadata']) for line in st.session_state['processed_files'][fp+'.pdf']]
+        with st.spinner(f"Creating new index for {selected_file}... "):
+            kb.upsert(documents,show_progress_bar=True)
+
+    context_engine = ContextEngine(kb)
+    chat_engine = ChatEngine(context_engine,allow_model_params_override=True)
+
 st.markdown("Chat with your document below")
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -45,20 +111,10 @@ if prompt := st.chat_input("What is up?"):
                 messages.append(SystemMessage(content=m['content']))
         
         res = chat_engine.chat(
-            #messages=[UserMessage(content=st.session_state.messages[-1]['content'])],
             messages=messages,
             stream=False, 
-            model_params={'model':'gpt-4-0125-preview'})
+            model_params={'model':'gpt-4-0125-preview','temperature':0,'seed':42})
         print(res)
         response = st.write(res.choices[0].message.content)
-        #stream = client.chat.completions.create(
-        #    model=st.session_state["openai_model"],
-        #    messages=[
-        #        {"role": m["role"], "content": m["content"]}
-        #        for m in st.session_state.messages
-        #    ],
-        #   stream=True,
-        #)
-        #response = st.write_stream(stream)
         
     st.session_state.messages.append({"role": "assistant", "content": res.choices[0].message.content})
