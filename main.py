e674e8cd963ec633cd38c600ac941482dd543133
Alex Eng <engalexm@gmail.com>
2024-04-17 08:57:12 -0400

    again

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ with col1:
         idx = idx.replace(' ','-').replace('_','-').lower()
         dct[idx] = item
 
-    pdf_viewer(dct[selected_file],height=800,width=800)
+    pdf_viewer(dct[selected_file],height=1000,width=800)
             
 with col2:
 
