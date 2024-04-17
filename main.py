7497df324e4b35dbd1d16c6e91e2ffd1c3f8f99d
Alex Eng <engalexm@gmail.com>
2024-04-17 00:52:47 -0400

    attempt 1

--- main.py
+++ main.py
@@ -75,7 +75,30 @@ with col1:
             
 with col2:
     if "messages" not in st.session_state:
-            st.session_state.messages = []
+        st.session_state.messages = []
+
+    if prompt := st.chat_input("What's on your mind?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message('user'):
+            st.markdown(prompt)
+        messages = []
+        for m in st.session_state.messages:
+            if m['role'] == 'user':
+                messages.append(UserMessage(content=m['content']))
+            else:
+                messages.append(SystemMessage(content=m['content']))
+        res = chat_engine.chat(
+            messages=messages,
+            stream=False, 
+            model_params={'model':'gpt-4-0125-preview','temperature':0,'seed':42})
+        
+        ans = res.choices[0].message.content
+        sources = re.findall(f'Source: {dct[selected_file]}: page \d*',ans,re.IGNORECASE)
+        for s in sources:
+            ans = ans.replace(s,f'**{s}**')
+        st.session_state.messages.append({"role": "assistant", "content": ans})
+        with st.chat_message('assistant'):
+            st.markdown(ans)
 
     with stylable_container(
         key="container_with_border",
@@ -139,26 +162,3 @@ with cols[0]:
                         processed_files[uploaded_file.name] = data
                         st.session_state['processed_files'] = processed_files
                     print("Reading Complete")
-
-with cols[1]:
-    if prompt := st.chat_input("What's on your mind?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message('user'):
-            st.markdown(prompt)
-        messages = []
-        for m in st.session_state.messages:
-            if m['role'] == 'user':
-                messages.append(UserMessage(content=m['content']))
-            else:
-                messages.append(SystemMessage(content=m['content']))
-        res = chat_engine.chat(
-            messages=messages,
-            stream=False, 
-            model_params={'model':'gpt-4-0125-preview','temperature':0,'seed':42})
-        
-        ans = res.choices[0].message.content
-        sources = re.findall(f'Source: {dct[selected_file]}: page \d*',ans,re.IGNORECASE)
-        for s in sources:
-            ans = ans.replace(s,f'**{s}**')
-        st.session_state.messages.append({"role": "assistant", "content": ans})
-        st.rerun()
